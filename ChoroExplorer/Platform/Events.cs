7f8a138ce08cef698948c17dbbbb3084c6987c4d
Bill Swanson <bswanson58@gmail.com>
2023-04-20 10:04:39 -0500

    Added a status view to the main window.

--- ChoroExplorer/Platform/Events.cs
+++ ChoroExplorer/Platform/Events.cs
@@ -1,5 +1,14 @@
 ﻿namespace ChoroExplorer.Platform {
     internal class Events {
+        public class StatusEvent {
+            public	string			Message { get; }
+            public	bool			ExtendDisplay { get; set; }
+
+            public StatusEvent( string message ) {
+                Message = message;
+            }
+        }
+
         public class DisplayExplorerRequest {
             public string	Target { get; }
 
