1c01016b9426279fdf81ac0f51fbeb7bf4c655ec
Bill Swanson <bswanson58@gmail.com>
2023-04-23 14:45:32 -0500

    Added a map control view with the ability to adjust the region color transparency.

---
+++ ChoroExplorer/MapDisplay/MapControlView.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ChoroExplorer.MapDisplay {
+    /// <summary>
+    /// Interaction logic for MapControlView.xaml
+    /// </summary>
+    public partial class MapControlView {
+        public MapControlView() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ChoroExplorer/MapDisplay/MapControlViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ChoroExplorer.Regions;
+using Fluxor;
+using ReusableBits.Wpf.ViewModelSupport;
+
+namespace ChoroExplorer.MapDisplay {
+    // ReSharper disable once ClassNeverInstantiated.Global
+    internal class MapControlViewModel : PropertyChangeBase {
+        private readonly IState<RegionState>    mRegionState;
+        private readonly IRegionsFacade         mRegionsFacade;
+
+        public MapControlViewModel( IState<RegionState> regionState, IRegionsFacade regionsFacade ) {
+            mRegionState = regionState;
+            mRegionsFacade = regionsFacade;
+
+            mRegionState.StateChanged += OnRegionStateChanged;
+        }
+
+        private void OnRegionStateChanged( object ? sender, EventArgs e ) {
+            RaisePropertyChanged( () => RegionColorTransparency );
+        }
+
+        public int RegionColorTransparency {
+            get => mRegionState.Value.RegionColorTransparency;
+            set => mRegionsFacade.SetRegionColorTransparency( value );
+        }
+    }
+}
