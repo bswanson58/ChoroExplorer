1e5ec3f955c40f26647dfe4bfca7b03f245998bc
Bill Swanson <bswanson58@gmail.com>
2023-04-25 12:42:27 -0500

    Added the user interface for adjusting the weight of a fact.

--- ChoroExplorer/Facts/FactList/FactViewModel.cs
+++ ChoroExplorer/Facts/FactList/FactViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using ChoroExplorer.Models;
 using ReusableBits.Wpf.ViewModelSupport;
 using System;
+using System.Windows.Input;
+using ReusableBits.Wpf.Commands;
 
 namespace ChoroExplorer.Facts.FactList {
     internal class FactViewModel : PropertyChangeBase {
@@ -13,9 +15,17 @@ namespace ChoroExplorer.Facts.FactList {
         public  string          FactKey => Data.FactKey;
         public  string          FactName => Data.FactName;
 
+        public  int             FactWeight { get; set; }
+
+        public  ICommand        IncrementWeight { get; }
+        public  ICommand        DecrementWeight { get; }
+
         public FactViewModel( FactData data, Action<FactViewModel> onChange ) {
             Data = data;
             Parameters = new FactParameters( data );
+
+            IncrementWeight = new DelegateCommand( OnIncrementWeight );
+            DecrementWeight = new DelegateCommand( OnDecrementWeight );
 
             mOnParameterChange = onChange;
         }
@@ -41,7 +51,20 @@ namespace ChoroExplorer.Facts.FactList {
             set {
                 Parameters = Parameters.With( value );
 
+                RaisePropertyChanged( () => Weight );
                 mOnParameterChange.Invoke( this );
+            }
+        }
+
+        private void OnIncrementWeight() {
+            if( Weight < 100 ) {
+                Weight++;
+            }
+        }
+
+        private void OnDecrementWeight() {
+            if( Weight > 0 ) {
+                Weight--;
             }
         }
     }
