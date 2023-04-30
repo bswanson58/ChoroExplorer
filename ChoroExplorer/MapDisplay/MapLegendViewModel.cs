a11376e2bee4300023092c4eeae63cbdfbea47a8
Bill Swanson <bswanson58@gmail.com>
2023-04-30 13:08:49 -0500

    Placed tick marks on the legend for the score of each region.

--- ChoroExplorer/MapDisplay/MapLegendViewModel.cs
+++ ChoroExplorer/MapDisplay/MapLegendViewModel.cs
@@ -1,14 +1,48 @@
-﻿using System.Windows.Media;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Media;
 using ChoroExplorer.ColorMapping;
+using ChoroExplorer.Models;
+using ChoroExplorer.Regions;
+using Fluxor.Selectors;
+using ReusableBits.Wpf.Platform;
 
 namespace ChoroExplorer.MapDisplay {
+    internal class RegionScoreLegend {
+        public  string  RegionName { get; }
+        public  int     RegionScore { get; }
+
+        public RegionScoreLegend( RegionSummary regionSummary ) {
+            RegionName = regionSummary.RegionName;
+            RegionScore = (int)( regionSummary.RegionScore * 1000 );
+        }
+    }
+
     // ReSharper disable once ClassNeverInstantiated.Global
-    internal class MapLegendViewModel {
+    internal class MapLegendViewModel : IDisposable {
+        private readonly ISelectorSubscription<IReadOnlyList<RegionSummary>>    mRegionSummary;
 
-        public  LinearGradientBrush     LegendBrush { get; }
+        public  ObservableCollection<RegionScoreLegend> Regions { get; }
+        public  LinearGradientBrush                     LegendBrush { get; }
 
-        public MapLegendViewModel( IColorMapper colorMapper ) {
+        public MapLegendViewModel( IColorMapper colorMapper, IRegionSelectors regionSelectors ) {
+            mRegionSummary = regionSelectors.RegionSummarySelector();
+            mRegionSummary.StateChanged += OnRegionSummaryChanged;
+
+            Regions = new ObservableCollection<RegionScoreLegend>();
+
             LegendBrush = new LinearGradientBrush { GradientStops = colorMapper.MappingColors };
+        }
+
+        private void OnRegionSummaryChanged( object ? sender, EventArgs e ) {
+            Regions.Clear();
+            Regions.AddRange( mRegionSummary.Value.Where( r => r.Enabled ).Select( r => new RegionScoreLegend( r )));
+        }
+
+        public void Dispose() {
+            mRegionSummary.Dispose();
         }
     }
 }
